67963b0461a797bc08c4c7e480d3bdb408c4b8cb
Piotr Szczygieł <szczygiel.p@gmail.com>
2018-12-18 23:39:06 +0100

    use url addresses as static fields

--- src/main/java/air/pollution/AirPollutionService.java
+++ src/main/java/air/pollution/AirPollutionService.java
@@ -7,6 +7,12 @@ import java.util.Arrays;
 import java.util.List;
 
 class AirPollutionService {
+    private static final String apiAddress = "http://api.gios.gov.pl/pjp-api/rest/";
+    private static final String apiGetAirIndex = apiAddress + "aqindex/getIndex/";
+    private static final String apiGetAllSensors = apiAddress + "station/sensors/";
+    private static final String apiGetAllStations = apiAddress + "station/findAll";
+    private static final String apiGetSensorMeasurements = apiAddress + "data/getData/";
+
     private Gson gson;
     private HttpGet get;
 
@@ -17,26 +23,18 @@ class AirPollutionService {
     }
 
     List<JsonStation> getAllStations() throws IOException {
-        return Arrays.asList(gson.fromJson(
-                get.from("http://api.gios.gov.pl/pjp-api/rest/station/findAll"),
-                JsonStation[].class));
+        return Arrays.asList(gson.fromJson(get.from(apiGetAllStations), JsonStation[].class));
     }
 
     List<JsonSensor> getAllSensors(int stationId) throws IOException {
-        return Arrays.asList(gson.fromJson(get.from(
-                "http://api.gios.gov.pl/pjp-api/rest/station/sensors/" + stationId),
-                JsonSensor[].class));
+        return Arrays.asList(gson.fromJson(get.from(apiGetAllSensors + stationId), JsonSensor[].class));
     }
 
     JsonSensorMeasurements getSensorMeasurements(int sensorId) throws IOException {
-        return gson.fromJson(get.from(
-                "http://api.gios.gov.pl/pjp-api/rest/data/getData/" + sensorId),
-                JsonSensorMeasurements.class);
+        return gson.fromJson(get.from(apiGetSensorMeasurements + sensorId), JsonSensorMeasurements.class);
     }
 
     JsonAirIndex getAirIndex(int stationId) throws IOException {
-        return gson.fromJson(get.from(
-                "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/" + stationId),
-                JsonAirIndex.class);
+        return gson.fromJson(get.from(apiGetAirIndex + stationId), JsonAirIndex.class);
     }
 }
